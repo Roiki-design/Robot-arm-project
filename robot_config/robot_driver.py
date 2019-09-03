939e4a2983b4cd585d6ad5b31626acde6488d0db
Roiki-design <51856064+Roiki-design@users.noreply.github.com>
2019-09-03 15:53:44 +0300

    Update robot_driver.py

--- robot_config/robot_driver.py
+++ robot_config/robot_driver.py
@@ -7,35 +7,30 @@
 # 
 #
 import sys
-import zmq
+from robolink import *    # RoboDK's API
+from robodk import *      # Math toolbox for robots
+from machinekit import hal
+import linuxcnc
+
+import socket
+RDK = Robolink()
+
+#Parameters to connect to the robot is being set
+HOST = "10.10.10.5" #robot ip
+PORT = 30002 #port to connect to
+
+# robot and tool object is being set
+robot = RDK.Item('Select a robot', ITEM_TYPE_ROBOT)
+tool = RDK.Item('Gripper')
+
+
+RDK.setSimulationSpeed(1)
+robot.setSpeed(1500, 1500, 2000, 180)
+
+
 
 def main():
-    connect_to = "tcp://127.0.0.1:5555"
-    topics = "'joint.0','joint.1','Joint.2','joint.3','joint.4','Joint.5','joint.6'
-    if len (sys.argv) > 1:
-        connect_to = sys.argv[1]
-    if len (sys.argv) > 2:
-        topics = sys.argv[2:]
-
-    ctx = zmq.Context()
-    s = ctx.socket(zmq.SUB)
-    s.connect(connect_to)
-
-    # manage subscriptions
-    if not topics:
-        print "Receiving messages on ALL topics..."
-        s.setsockopt(zmq.SUBSCRIBE,'')
-    else:
-        print "Receiving messages from topics: %s ..." % topics
-        for t in topics:
-            s.setsockopt(zmq.SUBSCRIBE,t)
-    try:
-        while True:
-            topic, msg = s.recv_multipart()
-            print 'topic: %s, msg:%s' % (topic, msg)
-    except KeyboardInterrupt:
-        pass
-    print "Done."
+    
 
 if __name__ == "__main__":
     main()
